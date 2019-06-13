990aa64e5c5e37f50fb317137d7fa4cc446e9834
lushilling <lshilling@hotmail.co.uk>
2019-06-13 17:08:10 +0100

    created a Dancer class

---
+++ src/main/java/com/qa/persistence/domain/Dancer.java
@@ -0,0 +1,75 @@
+package com.qa.persistence.domain;
+
+public class Dancer {
+
+	private int id;
+	private String name;
+	private String email;
+	private String ageGroup;
+	private String gradeName;
+	private String location;
+	
+	public Dancer(int id, String name, String email, String ageGroup, String gradeName, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.ageGroup = ageGroup;
+		this.gradeName = gradeName;
+		this.location = location;
+	}
+	
+	public Dancer() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAgeGroup() {
+		return ageGroup;
+	}
+
+	public void setAgeGroup(String ageGroup) {
+		this.ageGroup = ageGroup;
+	}
+
+	public String getGradeName() {
+		return gradeName;
+	}
+
+	public void setGradeName(String gradeName) {
+		this.gradeName = gradeName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+}
