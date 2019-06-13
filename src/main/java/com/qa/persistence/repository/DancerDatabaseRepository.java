a61f550ae7611258ac0c43fd1b52c454c709eae3
lushilling <lshilling@hotmail.co.uk>
2019-06-13 17:22:42 +0100

    created a dancerRepository Interface and a dancerDatabaseRepo class that implements the interface

---
+++ src/main/java/com/qa/persistence/repository/DancerDatabaseRepository.java
@@ -0,0 +1,36 @@
+package com.qa.persistence.repository;
+
+public class DancerDatabaseRepository implements DancerRepository {
+
+
+	public String getAllDancers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String getADacner(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String addADancer(String dancer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public String deleteADancer(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateDancerDetails(int id, String dancer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
---
+++ src/main/java/com/qa/persistence/repository/DancerRepository.java
@@ -0,0 +1,11 @@
+package com.qa.persistence.repository;
+
+public interface DancerRepository {
+	
+	String getAllDancers();
+	String getADacner(int id);
+	String addADancer(String dancer);
+	String deleteADancer(int id);
+	String updateDancerDetails(int id, String dancer);
+
+}
