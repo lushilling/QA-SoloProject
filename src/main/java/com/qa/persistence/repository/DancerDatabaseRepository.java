b9a7a651472de5b2b5d8665628a58b840b29db98
lushilling <lshilling@hotmail.co.uk>
2019-06-13 17:56:41 +0100

    finsihed writing methods for the dancerDatabaseRepo

--- src/main/java/com/qa/persistence/repository/DancerDatabaseRepository.java
+++ src/main/java/com/qa/persistence/repository/DancerDatabaseRepository.java
@@ -1,36 +1,57 @@
 package com.qa.persistence.repository;
 
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import com.qa.persistence.domain.Dancer;
+import com.qa.utility.JSONUtil;
+
+@Transactional(value = TxType.SUPPORTS)
 public class DancerDatabaseRepository implements DancerRepository {
 
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
 
 	public String getAllDancers() {
-		// TODO Auto-generated method stub
-		return null;
+		Query getAllQuery = manager.createQuery("Select d FROM Dancer d");
+		Collection<Dancer> dancer = (Collection<Dancer>) getAllQuery.getResultList();
+		return util.getJSONForObject(dancer);
 	}
-
 
 	public String getADacner(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return util.getJSONForObject(manager.find(Dancer.class, id));
 	}
 
-
+	@Transactional(value = TxType.REQUIRED)
 	public String addADancer(String dancer) {
-		// TODO Auto-generated method stub
-		return null;
+		manager.persist(dancer);
+		return dancer;
 	}
 
-
+	@Transactional(value = TxType.REQUIRED)
 	public String deleteADancer(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		manager.remove(manager.find(Dancer.class, id));
+		return util.returnMessage("Dancers details deleted");
 	}
 
+	@Transactional(value = TxType.REQUIRED)
 	public String updateDancerDetails(int id, String dancer) {
-		// TODO Auto-generated method stub
-		return null;
+		Dancer updatedDancer = util.getObjectForJSON(dancer, Dancer.class);
+		Dancer oldDancer = manager.find(Dancer.class, id);
+		oldDancer.setAgeGroup(updatedDancer.getAgeGroup());
+		oldDancer.setEmail(updatedDancer.getEmail());
+		oldDancer.setGradeName(updatedDancer.getGradeName());
+		oldDancer.setLocation(updatedDancer.getLocation());
+		oldDancer.setName(updatedDancer.getName());
+		manager.persist(oldDancer);
+		return util.returnMessage("Dancers details are updated");
 	}
-
-
 
 }
