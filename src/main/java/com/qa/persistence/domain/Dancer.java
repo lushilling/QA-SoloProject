03310f7b0df172efd0a35237460d67e9124b77e7
lushilling <lshilling@hotmail.co.uk>
2019-06-17 12:18:53 +0100

    deployed

--- src/main/java/com/qa/persistence/domain/Dancer.java
+++ src/main/java/com/qa/persistence/domain/Dancer.java
@@ -2,10 +2,12 @@ package com.qa.persistence.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Dancer {
 
+	@Id
 	@GeneratedValue
 	private int id;
 	private String name;
