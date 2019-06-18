78d7a67c1e66c9039b8e8fe331e2feecde27ae19
lushilling <lshilling@hotmail.co.uk>
2019-06-18 17:26:50 +0100

    updated

--- src/main/java/com/qa/persistence/domain/Dancer.java
+++ src/main/java/com/qa/persistence/domain/Dancer.java
@@ -2,13 +2,14 @@ package com.qa.persistence.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Dancer {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
